3a685bf6d541eb8f2742236d6e066a3907780384
exclucivelogin <alter4444@gmail.com>
2021-05-05 13:23:40 +0400

    canopy models add

--- entity_repo.ts
+++ entity_repo.ts
@@ -10,4 +10,15 @@ export const entityRepo: EntityRepo = {
             { key: 'description', type: 'string', title: 'Описание', required: false},
         ]
     },
+    canopy_model: {
+        db_name: 'canopy_model',
+        fields:[
+            { key: 'id', title: 'ID', type: 'id', readonly: true },
+            { key: 'slug', type: 'string', title: 'Ключ', required: true},
+            { key: 'name', type: 'string', title: 'Название', required: true},
+            { key: 'short_name', type: 'string', title: 'Короткое название', required: false},
+            { key: 'title', type: 'string', title: 'Заоголовок', required: false},
+            { key: 'brand', type: 'string', title: 'Бренд', required: false},
+        ]
+    },
 };
